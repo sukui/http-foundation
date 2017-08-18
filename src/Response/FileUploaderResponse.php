eb35f07231667132944746915855011e3b910718
chuxiaofeng <chuxiaofeng@youzan.com>
2017-08-18 13:06:48 +0800

    add upload

---
+++ src/Response/FileUploaderResponse.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ZanPHP\HttpFoundation\Response;
+
+use ZanPHP\Contracts\Http\ResponseTrait;
+use ZanPHP\Contracts\Network\Response;
+use swoole_http_response as SwooleHttpResponse;
+use ZanPHP\Exception\System\InvalidArgumentException;
+use ZanPHP\HttpFoundation\Exception\FileUploadFailException;
+
+class FileUploaderResponse extends BaseResponse implements Response
+{
+    use ResponseTrait;
+
+    public function __construct($filepath, $status = 200, array $headers = [])
+    {
+        parent::__construct($filepath, $status, $headers);
+    }
+
+    public function sendBy(SwooleHttpResponse $swooleHttpResponse)
+    {
+        $this->sendHeadersBy($swooleHttpResponse);
+        $filepath = realpath($this->content);
+        if (!$filepath || !is_readable($filepath)) {
+            throw new InvalidArgumentException("Invalid uploading file : $filepath");
+        }
+        if (!$swooleHttpResponse->sendfile($this->content)) {
+            throw new FileUploadFailException("Failed to upload file: $filepath");
+        }
+        return $this;
+    }
+}
