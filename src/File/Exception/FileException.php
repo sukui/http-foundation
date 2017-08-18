a5bf270dcbce8cf2eeb492cdde41a693dddcd222
chuxiaofeng <chuxiaofeng@youzan.com>
2017-08-18 17:00:09 +0800

    add files

---
+++ src/File/Exception/FileException.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ZanPHP\HttpFoundation\File\Exception;
+
+use ZanPHP\Exception\ZanException;
+
+/**
+ * Thrown when an error occurred in the component File.
+ *
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class FileException extends ZanException
+{
+}
